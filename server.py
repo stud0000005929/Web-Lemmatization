b7931eb9da931b13bc945e0008381da00e8a06cf
Manakov <manakov@42dev.ru>
2023-11-21 20:13:49 +0500

    Changed st

--- server.py
+++ server.py
@@ -5,13 +5,24 @@ st.set_page_config(layout="centered")
 
 st.title('Программаня инженерия')
 
-text = st.text_input('Введите строку:')
+text = st.text_area('Введите строку:')
 words = text.split()
 
 df = pd.DataFrame({'Words': words})
 
-if st.button('Вывести резуьтат'):
-    st.success('Yay!')
-    st.dataframe(df)
-    output_csv = df.to_csv(index=False)
-    st.download_button(label='Download CSV', data=output_csv, file_name='output_file.csv', mime='text/csv')
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.write(' ')
+
+with col2:
+    if st.button('Вывести резуьтат'):
+        st.success('Yay!')
+        st.dataframe(df)
+        output_csv = df.to_csv(index=False)
+        st.download_button(label='Download CSV', data=output_csv, file_name='output_file.csv', mime='text/csv')
+
+with col3:
+    st.write(' ')
+
+
