08edccbfcade43e4b5bf25d593386a0fe329c0c9
RoGr <96972624+Your-RoGr@users.noreply.github.com>
2023-12-04 19:34:54 +0500

    v1.0.1

--- model.py
+++ model.py
@@ -1,7 +1,8 @@
 import spacy
+import pandas as pd
 
 
-def init_model(model_name):
+def init_model(model_name='ru_core_news_md'):
     try:
         nlp = spacy.load(model_name)
         return nlp
@@ -9,3 +10,18 @@ def init_model(model_name):
         spacy.cli.download(model_name)
         nlp = spacy.load(model_name)
         return nlp
+
+def get_json(text, model_name='ru_core_news_md'):
+
+    nlp = init_model(model_name)
+
+    doc = nlp(text)
+
+    dfLemma = pd.DataFrame({'text': [token.text for token in doc],
+                            'pos': [token.pos_ for token in doc],
+                            'dep': [token.dep_ for token in doc]})
+
+    return dfLemma.to_json(force_ascii=False)
+
+def get_str(text, model_name='ru_core_news_md'):
+    return str(get_json(text, model_name))
